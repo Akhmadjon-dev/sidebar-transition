a75298f36c18fb1b0b7790b675e5ba587a179faf
Akhmadjon-dev <info.ahmadjon@gmail.com>
2021-05-04 16:24:11 +0500

    hover added to content

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 function openSlideMenu() {
   document.getElementById("menu").style.width = "250px";
-  // document.getElementById("content").style.marginLeft = "250px";
+  // document.getElementById("hover").style.cssText =
+  //   "opacity: 1;  transition: 0.5s ease-in;";
+  document.getElementById("hover").classList.add("show");
 }
 function closeSlideMenu() {
   document.getElementById("menu").style.width = "0";
-  // document.getElementById("content").style.marginLeft = "0";
+  document.getElementById("hover").classList.remove("show");
+  // document.getElementById("hover").style.cssText = "opacity: 0;   ease-in;";
 }
